56cfe16882453e633f2fd4e6c3076d8b123b5b3c
AvanRocks <64660241+AvanRocks@users.noreply.github.com>
2021-06-30 16:52:00 -0400

    test

--- src/server/express/controllers/user/createUser.js
+++ src/server/express/controllers/user/createUser.js
@@ -4,24 +4,31 @@ const isDiscordNameValid = require("../../../../db/utils/isDiscordNameValid");
 
 async function createUser(req, res) {
   const { name, discordName } = req.body;
+  console.log(name);
+  console.log(discordName);
 
   if (!name || !discordName) {
     return res.status(400).send("Please provide a name and discordName");
   }
 
+  console.log("here");
   let discordNameIsValid = await isDiscordNameValid(discordName);
+  console.log("there");
   if (!discordNameIsValid) {
     return res.status(400).send("Discord username taken");
   }
 
   let token = crypto.randomBytes(40).toString("hex");
 
+  console.log("over here");
   await db.query(
     "INSERT INTO user_accounts (name, discordName, token) VALUES ($1, $2, $3)",
     [name, discordName, token]
   );
+  console.log("over there");
 
   res.send(token);
+  console.log("done");
 }
 
 module.exports = createUser;
