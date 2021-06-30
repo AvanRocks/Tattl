3b1fbd340ebf327d26d1c5b64e9835da532c2053
AvanRocks <64660241+AvanRocks@users.noreply.github.com>
2021-06-30 14:48:48 -0400

    only allow https

--- src/server/express/index.js
+++ src/server/express/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const router = require("./routes");
+const rejectHttp = require("./middleware/rejectHttp");
 
+app.use(rejectHttp);
 app.use("/", router);
 
 module.exports = app;
---
+++ src/server/express/middleware/rejectHttp.js
@@ -0,0 +1,9 @@
+function rejectHttp(req, res, next) {
+  if (req.protocol !== "https") {
+    return res.status(403).send({ message: "SSL required" });
+  }
+
+  next();
+}
+
+module.exports = rejectHttp;
