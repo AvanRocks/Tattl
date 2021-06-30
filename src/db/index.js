e427984a57e96487045482069b6143c470118525
AvanRocks <64660241+AvanRocks@users.noreply.github.com>
2021-06-30 17:01:08 -0400

    test

--- src/db/index.js
+++ src/db/index.js
@@ -3,14 +3,17 @@ const config = require("./config");
 const pool = new Pool(config);
 console.log(pool);
 
-try {
-  const dbRes = await pool.query("SELECT * from user_accounts");
-  console.log("worked");
-  console.log(dbRes);
-} catch (err) {
-  console.log(err);
-  console.log(err.message);
+async function test() {
+  try {
+    const dbRes = await pool.query("SELECT * from user_accounts");
+    console.log("worked");
+    console.log(dbRes);
+  } catch (err) {
+    console.log(err);
+    console.log(err.message);
+  }
 }
+test();
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
