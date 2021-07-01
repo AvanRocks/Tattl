1f09e21a99dd69ea6217ff6815e76be4262a8a62
AvanRocks <64660241+AvanRocks@users.noreply.github.com>
2021-06-30 20:19:55 -0400

    help

--- src/bot/commands/help.js
+++ src/bot/commands/help.js
@@ -2,9 +2,9 @@ const { client } = require("../client");
 const { sendMsg } = require("../utils/sendMsg");
 
 const helpTxt = `
-help    Display a help message
-check   Check if the bot is running
-listMembers   List all users registered with accountability bot
+help                Display a help message
+check               Check if the bot is running
+listMembers         List all users registered with accountability bot
 listOnlineMembers   List all the members whose tattle browser extension is connected
 `;
 
