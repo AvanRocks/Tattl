05ba4ebee794d19cb7c26a2ac2d3eedd3da92836
AvanRocks <64660241+AvanRocks@users.noreply.github.com>
2021-06-30 20:07:14 -0400

    Revert "help command"
    This reverts commit 533e1a915bd53608dc1a666908fc127ca15acff3.

--- src/db/index.js
+++ src/db/index.js
@@ -1,6 +1,7 @@
 const { Pool } = require("pg");
 const config = require("./config");
 const pool = new Pool(config);
+console.log(pool);
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
