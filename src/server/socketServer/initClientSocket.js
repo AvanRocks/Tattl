f14518448cf7c06aa9a7187382eefd30ec9b6270
AvanRocks <64660241+AvanRocks@users.noreply.github.com>
2021-07-07 10:42:36 -0400

    remove debug

--- src/server/socketServer/initClientSocket.js
+++ src/server/socketServer/initClientSocket.js
@@ -3,7 +3,6 @@ const getUserId = require("../../db/utils/getUserId");
 async function setUserId(socket) {
   let token = socket.handshake.auth.token;
   const userId = await getUserId("token", token);
-  console.log(`user connected with userId: ${userId}`);
   socket.userId = userId;
 }
 
